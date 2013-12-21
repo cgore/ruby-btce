f357d83f69df0027e4ffcaabb26e1eec84119084
Selvam Palanimalai <selvam1991@gmail.com>
2013-12-21 19:21:35 +0530

    fee and ticker response object handling changed to accomodate api v3.

--- lib/btce/api/operations/ticker.rb
+++ lib/btce/api/operations/ticker.rb
@@ -38,12 +38,12 @@ module Btce
       super 'ticker', pair
     end
 
-    JSON_METHODS = %w(high low avg vol vol_cur last buy sell server_time)
+    JSON_METHODS = %w(high low avg vol vol_cur last buy sell updated)
 
     JSON_METHODS.each do |method|
       class_eval %{
         def #{method}
-          json["ticker"]["#{method}"] if json["ticker"] and json["ticker"].is_a? Hash
+          json[@pair]["#{method}"] if json[@pair] and json[@pair].is_a? Hash
         end
       }
     end
